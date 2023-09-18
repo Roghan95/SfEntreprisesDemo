bdc150a714c4b1f6ff928efb9ac7467078b3f20f
Roghan95 <p90svborz@gmail.com>
2023-09-18 16:18:03 +0200

    Affichage liste employes et entreprises dans les vues

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Entreprise;
+use App\Repository\EntrepriseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +12,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class EntrepriseController extends AbstractController
 {
     #[Route('/entreprise', name: 'app_entreprise')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntrepriseRepository $entrepriseRepository): Response
     {
-        $entreprises = $entityManager->getRepository(Entreprise::class)->findAll();
-
+        $entreprises = $entrepriseRepository->findBy(["ville" => "STRASBOURG"], ["raisonSociale" => "ASC"]);
         return $this->render('entreprise/index.html.twig', [
             'entreprises' => $entreprises
         ]);
     }
-}
+} 
