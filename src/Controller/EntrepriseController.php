833185302059e91931dc6d0dd5a0fa6361025fa3
Roghan95 <p90svborz@gmail.com>
2023-09-18 15:37:23 +0200

    Affichage de la liste d'entreprises

---
+++ src/Controller/EntrepriseController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Entreprise;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class EntrepriseController extends AbstractController
+{
+    #[Route('/entreprise', name: 'app_entreprise')]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $entreprises = $entityManager->getRepository(Entreprise::class)->findAll();
+
+        return $this->render('entreprise/index.html.twig', [
+            'entreprises' => $entreprises
+        ]);
+    }
+}
